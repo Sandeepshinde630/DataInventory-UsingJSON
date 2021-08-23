7dbb385264e7318ddc6c47c6c1a835f0f5217a68
unknown <sandeepshinde630@gmail.com>
2021-08-23 12:44:49 +0530

    feat:Added Data inventory problem

---
+++ JSON/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to JSON Inventory Data Management");
+            InventoryData inventoryData = new InventoryData();
+        }
+    }
+}
